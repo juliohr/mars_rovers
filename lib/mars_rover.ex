d566f5e5029d9b1f7b42e908ae790a26a291b6de
Julio-Home <julio.ss@gmail.com>
2016-11-21 17:35:33 -0200

    implements rover current position

--- lib/mars_rover.ex
+++ lib/mars_rover.ex
@@ -1,2 +1,12 @@
 defmodule MarsRover do
+
+	def create(rover) do
+		{ :ok, rover } = Agent.start_link(fn -> %Rover{ x: rover.x, y: rover.y, orientation: rover.orientation} end)
+	end
+
+	def current_position(rover) do
+		Agent.get(rover, fn rover ->
+			%{ x: rover.x, y: rover.y, orientation: rover.orientation } 
+		end)
+	end
 end

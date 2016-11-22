1158ed3976772fbe776b50caf57769997711ea81
Julio-Home <julio.ss@gmail.com>
2016-11-22 10:21:01 -0200

    cleans up code

--- lib/mars_rover.ex
+++ lib/mars_rover.ex
@@ -1,6 +1,6 @@
 defmodule MarsRover do
   def create(rover) do
-    { :ok, rover } = Agent.start_link(fn -> %Rover{ x: rover.x, y: rover.y, orientation: rover.orientation} end)
+    Agent.start_link(fn -> %Rover{ x: rover.x, y: rover.y, orientation: rover.orientation} end)
   end
 
   def current_position(rover) do
